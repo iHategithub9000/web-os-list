1b4ef7d678dd5440ce9ba6d478b9078de455ba0c
GithubDestroyer.py <101750090+iHategithub9000@users.noreply.github.com>
2023-05-03 20:01:48 +0200

    Added console, removed "taskbar"

---
+++ the/console.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  ct = document.getElementById("consoletext")
+  co = document.getElementById("cout")
+  ca = document.getElementById("caccept")
+
+  ca.addEventListener("click",()=>{co.text = "Output:\n"+eval(ct.text.trim())});
+})
