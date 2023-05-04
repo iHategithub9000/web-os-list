f421840ab779617c9acdbee7aae3d4a92c00c9d8
GithubDestroyer.py <101750090+iHategithub9000@users.noreply.github.com>
2023-05-04 21:20:41 +0200

    Update opensrcfs.js

--- webofs/opensrcfs.js
+++ webofs/opensrcfs.js
@@ -1,1 +1,1 @@
-webofs={};fetch("https://ihategithub9000.github.io/web-os-centre/webofs/fs.js").then(res=>res.text()).then(eval);FS=webofs.FS;webofs=null
+let setup_fs = async function(){let webofs={};await fetch("https://ihategithub9000.github.io/web-os-centre/webofs/fs.js").then(res=>res.text()).then(eval);FS=webofs.FS;webofs=null};setup_fs()
