d1c36ab78d904daf8f6b9d3b46750e885151b960
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2024-01-05 11:41:44 +0100

    Delete whole f*cking webos

--- webofs/opensrcfs.js
+++
@@ -1,1 +0,0 @@
-let setup_fs = async function(){let webofs={};await fetch("https://ihategithub9000.github.io/web-os-centre/webofs/fs.js").then(res=>res.text()).then(eval);FS=webofs.FS;webofs=null};setup_fs()
--- webofs/webofs_assign.js
+++
@@ -1,2 +0,0 @@
-console.log("webofs assigning")
-webofs={kload:{}}
